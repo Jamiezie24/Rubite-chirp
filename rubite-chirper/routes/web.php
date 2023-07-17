305ae633bae3b201fdd35dafaa31dd056902202b
Jamiezie24 <138562110+Jamiezie24@users.noreply.github.com>
2023-07-17 11:25:54 +0800

    Update web.php

--- rubite-chirper/routes/web.php
+++ rubite-chirper/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ChirpController;
+use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('welcome');
 });
 
